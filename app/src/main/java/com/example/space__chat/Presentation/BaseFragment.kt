dd2d17af119d2ad17e51911df2d71288dca70f20
Nini <qoqranini@gmail.com>
2023-04-18 14:09:53 +0400

    added basic design, Fragments, RecyclerView Items, Landscape mode

---
+++ app/src/main/java/com/example/space__chat/Presentation/BaseFragment.kt
@@ -0,0 +1,38 @@
+package com.example.space__chat.Presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+typealias Inflate<VB> = (LayoutInflater, ViewGroup, Boolean) -> VB
+
+abstract class BaseFragment<VB : ViewBinding> : Fragment() {
+
+    private var _binding: VB? = null
+    abstract fun inflate(): Inflate<VB>
+    protected val binding get() = _binding!!
+
+    abstract fun onBind()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = this.inflate().invoke(inflater, container!!, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onBind()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/space__chat/Presentation/MziaScreen/MziaViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.space__chat.Presentation.MziaScreen
+
+import androidx.lifecycle.ViewModel
+
+class MziaViewModel :ViewModel() {
+}
