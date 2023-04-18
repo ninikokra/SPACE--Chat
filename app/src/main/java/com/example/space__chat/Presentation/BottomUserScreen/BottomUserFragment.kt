948e7568e3cee9ebdc4d307c0e1803201da98447
Nini <qoqranini@gmail.com>
2023-04-18 19:55:53 +0400

    Changed id, dimensions,fragments init function

--- app/src/main/java/com/example/space__chat/Presentation/BottomUserScreen/BottomUserFragment.kt
+++ app/src/main/java/com/example/space__chat/Presentation/BottomUserScreen/BottomUserFragment.kt
@@ -2,16 +2,16 @@ package com.example.space__chat.Presentation.BottomUserScreen
 
 
 import androidx.fragment.app.viewModels
-import com.example.space__chat.Presentation.BaseFragment
-import com.example.space__chat.Presentation.Inflate
-import com.example.space__chat.databinding.BottomUserItemBinding
+import com.example.space__chat.Presentation.BASE.BaseFragment
+import com.example.space__chat.Presentation.BASE.Inflate
+import com.example.space__chat.databinding.FragmentBottomUserBinding
 
 
-class BottomUserFragment : BaseFragment<BottomUserItemBinding>() {
+class BottomUserFragment : BaseFragment<FragmentBottomUserBinding>() {
     private val viewModel: BottomUserViewModel by viewModels()
 
-    override fun inflate(): Inflate<BottomUserItemBinding> {
-        return BottomUserItemBinding::inflate
+    override fun inflate(): Inflate<FragmentBottomUserBinding> {
+        return FragmentBottomUserBinding::inflate
     }
 
     override fun onBind() {
