3c1081d1651a4646904de4034c9971ff14d52695
Nini <qoqranini@gmail.com>
2023-05-15 15:44:28 +0400

    refactor documentation

--- app/src/main/java/com/space/space__chat/presentation/vm/ChatUsersViewModel.kt
+++ app/src/main/java/com/space/space__chat/presentation/vm/ChatUsersViewModel.kt
@@ -13,6 +13,7 @@ import kotlinx.coroutines.flow.Flow
  */
 
 class ChatUsersViewModel(private val chatRepository: ChatRepository) : ViewModel() {
+
     /**
      * [showMessages]Retrieves and returns a flow of the list of [MessageModel] representing the messages to be shown.
      * @return A [Flow] emitting a list of [MessageModel] objects representing the messages.
@@ -21,6 +22,8 @@ class ChatUsersViewModel(private val chatRepository: ChatRepository) : ViewModel
 
     /**
      *[filterMessages] Filters the given list of [MessageModel] based on the provided listener.
+     * @param messages The list of [MessageModel] to be filtered.
+     * @param listener The listener's name used for filtering.
      * @return A new list of [MessageModel] that includes messages where the sender matches the listener or
      * the sender is online.
      */
@@ -32,6 +35,9 @@ class ChatUsersViewModel(private val chatRepository: ChatRepository) : ViewModel
 
     /**
      * [provideMessageModel]Creates and returns a new [MessageModel] object based on the provided input parameters.
+     * @param editTextInput The content of the message.
+     * @param userId The ID of the user sending the message.
+     * @param isOnline Flag indicating whether the sender is currently online.
      * @return A new [MessageModel] object representing the message with the provided input.
      */
     private fun provideMessageModel(editTextInput: String, userId: String, isOnline: Boolean) =
@@ -44,6 +50,9 @@ class ChatUsersViewModel(private val chatRepository: ChatRepository) : ViewModel
 
     /**
      * [sendMessage]Sends a message with the provided input to a user identified by the user ID.
+     * @param messageInput The content of the message to be sent.
+     * @param userId The ID of the user who will receive the message.
+     * @param isOnline Flag indicating whether the user is currently online.
      */
 
     fun sendMessage(messageInput: String, userId: String, isOnline: Boolean) {
